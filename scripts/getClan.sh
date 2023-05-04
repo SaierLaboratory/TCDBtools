ee5ef793cf1a747754fc09763e0c36b0a6240fd2
Arturo Medrano <l1medranosoto@ucsd.edu>
2023-05-03 17:35:24 -0700

    Cleaned repository

--- scripts/getClan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Extract the clan info for any pfam accession
-zgrep "$1" /ResearchData/pfam/download/Pfam-A.clans.tsv.gz
-
--- scripts/run_pfam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#==========================================================================
-# Arguments:
-#   1. File with input sequences in fasta format
-#   2. output file name
-
-hmmscan --cpu 4 --noali --cut_ga -o /dev/null --domtblout $2 /ResearchData/pfam/pfamdb/Pfam-A.hmm $1
